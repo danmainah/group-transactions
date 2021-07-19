2f3d6d7364f2ad62d741f2677152b847256482f9
unknown <danielmainawaweru@gmail.com>
2021-07-19 12:57:59 +0300

    Configure RESTFUL routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  "projects#index"
-  resources :users
-  resources :projects
-  resources :sessions
+  resources :users , only: [:create, :new]
+  resources :projects, only: [:index, :grouped, :show, :create, :new, :destroy, :edit]
+  resources :sessions, only: [:create, :new, :destroy]
   resources :groups do 
     resources :projects
   end
